b189b6f0a8ed81118088ef0284c91ce754a61eaf
gozzt <gouut@outlook.com>
2021-09-26 10:17:44 +0800

    另一种

--- 99.py
+++ 99.py
@@ -6,3 +6,13 @@ while i <= 9:
         j = j + 1
     print("\n")
     i = i + 1
+print("------------------------------------------")
+i = 9
+while i >= 1:
+    j = 9
+    while j >= 1:
+
+        print("%d*%d=%2d"%(i,j,i*j),end=" ")
+        j = j - 1
+    print("\n")
+    i = i - 1
